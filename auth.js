23867d2914e79600ca81dcbeaa473792b052f96f
fricksju <jfrickssoares@gmail.com>
2025-09-07 20:09:38 -0300

    atualizacoes

---
+++ auth.js
@@ -0,0 +1,13 @@
+function verificarAutenticacao(req, res, next) {
+  const { tipoUsuario } = req.body;
+
+  if (tipoUsuario === "ADM") {
+    return next(); 
+  }
+
+  return res.status(401).json({
+    mensagem: "Funcao nao permitida para esse usuario"
+  });
+}
+
+module.exports = verificarAutenticacao;
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const verificarAutenticacao = require("./auth");
+
+const app = express();
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log("Chamando API");
+  next();
+});
+
+app.get("/usuarios", (req, res) => {
+  const usuarios = [
+    { id: 1, nome: "Gabigol" },
+    { id: 2, nome: "Arrasca" },
+    { id: 3, nome: "Bruno Henrique" }
+  ];
+  res.json(usuarios);
+});
+
+
+app.post("/usuarios", verificarAutenticacao, (req, res) => {
+  res.status(201).json({ mensagem: "Rota permitida" });
+});
+
+
+app.listen(3000, () => {
+  console.log("Rodando na porta 3000");
+});
